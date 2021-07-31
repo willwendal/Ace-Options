323fc138b983442cc767f02574bce9ca6068c9ce
Andrés Olivé <andresolive@gmail.com>
2021-07-31 16:40:07 +0200

    react routing to show the chain component works, but the actual chain still doesn't render

--- client/src/Components/ListOption/ListOption.component.jsx
+++ client/src/Components/ListOption/ListOption.component.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'; 
+import OptionChain from '../OptionChain/OptionChain.component'
 
 export default function ListOption({ company }) {
   return (
-    <div className="ListOption">
+    <Router>
 
-      <h5>{company.symbol}</h5>
-      <h5>{company.description}</h5>
-    </div>
+      <div className="ListOption">
+
+        <nav>
+          <ul>
+            <li>
+              <Link to="/chain">{company.symbol}</Link>
+            </li>
+
+            <li>
+              <Link to="/chain">{company.description}</Link>
+            </li>
+          </ul>
+        </nav>
+
+      </div>
+
+      <Switch>
+        <Route path="/chain" component={() => <OptionChain company={company} /> } />
+        </Switch>
+
+        {/* <a href="/chain">{company.symbol}</a>
+        <p></p>
+        <a href="http://localhost:3000/chain">{company.description}</a>
+        <p></p> */}
+
+
+    </Router>
   )
 }
--- client/src/Components/OptionChain/OptionChain.component.jsx
+++ client/src/Components/OptionChain/OptionChain.component.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function OptionChain() {
+export default function OptionChain({ company }) {
   return (
     <div className="OptionChain">
-      
+      <h5>THE CHAIN IS WORKING!!! {company.symbol}</h5>
     </div>
   )
 }
