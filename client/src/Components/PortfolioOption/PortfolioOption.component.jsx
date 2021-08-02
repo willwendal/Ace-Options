c62c88793be06d1e19bcc8aa2a4742b848ad1372
Andrés Olivé <andresolive@gmail.com>
2021-08-02 20:08:28 +0200

    portfolio style good enough

--- client/src/Components/PortfolioOption/PortfolioOption.component.jsx
+++ client/src/Components/PortfolioOption/PortfolioOption.component.jsx
@@ -1,15 +1,35 @@
-
+import './PortfolioOption.css';
 
 export default function PortfolioOption({ option }) {
   return (
     <div className="PortfolioOption">
-      <div>Description: {option.description}</div>
-      <div>Option Type: {option.option_type}</div>
-      <div>Contract Size: {option.contract_size}</div>
-      <div>Buy Price: {option.ask}</div>
-      <div>Strike Price: {option.strike}</div>
-      <div>Expiration: {option.expiration_date}</div>
-      <div>P&L: {'TBD'}</div>
+
+      {/* <div id="portfolio-headers">
+        <div className="portfolio-headers description">Description</div>
+        <div className="portfolio-headers type">Type</div>
+        <div className="portfolio-headers bid">Bid</div>
+        <div className="portfolio-headers ask">Ask</div>
+        <div className="portfolio-headers buy-price">Buy Price</div>
+        <div className="portfolio-headers strike">Strike</div>
+        <div className="portfolio-headers exp-date">Exp.</div>
+        <div className="portfolio-headers p-l">P&L</div>
+      </div> */}
+
+      <div id="the-actual-portfolio">
+        <div className="portfolio-options description">{option.description.slice(0,4)}</div>
+        <div className="portfolio-options type">{option.option_type}</div>
+        <div className="portfolio-options bid">{(option.bid) ? option.bid : 0.00}</div>
+        <div className="portfolio-options ask">{(option.ask) ? option.ask : 0.00}</div>
+        <div className="portfolio-options buy-price">{Math.round(((option.ask*option.contract_size)*100) / 100).toFixed(2)}</div>
+        <div className="portfolio-options strike">{option.strike}</div>
+        <div className="portfolio-options exp-date">{option.expiration_date}</div>
+        <div className="portfolio-options money">{'TBD'}</div>
+        <div className="portfolio-options p-l">{'TBD'}</div>
+        <div className="portfolio-options sell">
+          <button className="portfolio-options sell-button">✌️</button>
+        </div>
+      </div>
+
     </div>
   )
 }
--- client/src/Components/WatchlistPortfolio/WatchlistPortfolio.component.jsx
+++ client/src/Components/WatchlistPortfolio/WatchlistPortfolio.component.jsx
@@ -48,9 +48,25 @@ export default function WatchlistPortfolio({ selectedView, userState, setUserSta
     <div className="WatchlistPortfolio">
 
       <h2 id="pf-wl-title">{titleState}</h2>
+
       
       {titleState === 'Portfolio' ? 
-        <div id="wl-or-pf">{portfolioItems}</div>
+
+        <div id="wl-or-pf">
+          <div id="portfolio-headers">
+            <div className="portfolio-headers description">Ticker</div>
+            <div className="portfolio-headers type">Type</div>
+            <div className="portfolio-headers bid">Bid</div>
+            <div className="portfolio-headers ask">Ask</div>
+            <div className="portfolio-headers buy-price">Buy Price</div>
+            <div className="portfolio-headers strike">Strike</div>
+            <div className="portfolio-headers exp-date">Exp.</div>
+            <div className="portfolio-headers exp-date">Money</div>
+            <div className="portfolio-headers p-l">P&L</div>
+            <div className="portfolio-headers sekk">Sell</div>
+          </div>
+          {portfolioItems}
+          </div>
       :
         <div id="wl-or-pf">{watchlistItems}</div>
       }
