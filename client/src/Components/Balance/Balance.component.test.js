9377e38e48dc01afd83276eea1fc736fded82752
Alex <19alexferrante99@gmail.com>
2021-08-06 20:19:52 +0200

    balance test finished

--- client/src/Components/Balance/Balance.component.test.js
+++ client/src/Components/Balance/Balance.component.test.js
@@ -1,58 +1,41 @@
-import React from 'react';
+import React from 'react'
+import Balance from './Balance.component.jsx'
+import { render, cleanup } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
 
-import Balance from './Balance.component.jsx';
+describe('Balance', () => {
+  const mockedUser = {
+    firstName: 'Jhon',
+    lastName: 'Doe',
+    balance: 1000000,
+  }
 
-import { render, cleanup } from '@testing-library/react';
+  afterEach(() => {
+    cleanup()
+  })
 
-import '@testing-library/jest-dom/extend-expect';
+  test('It should render positive balance in the right format', () => {
+    const { getByTestId } = render(<Balance userState={mockedUser} />)
+    const balanceDiv = getByTestId('test-balance-box')
 
-const mockedUser = {
-  firstName: 'Jhon',
-  lastName: 'Doe',
-  balance: 1000000
-}
+    expect(balanceDiv).toHaveTextContent('$1,000,000.00')
+  })
 
-afterEach(() => {
-  cleanup();
+  test('It should render negative balance in the right format', () => {
+    mockedUser.balance = -1000000
+
+    const { getByTestId } = render(<Balance userState={mockedUser} />)
+    const balanceDiv = getByTestId('test-balance-box')
+
+    expect(balanceDiv).toHaveTextContent('-$1,000,000.00')
+  })
+
+  test('It should render no balance in the right format', () => {
+    mockedUser.balance = 0
+
+    const { getByTestId } = render(<Balance userState={mockedUser} />)
+    const balanceDiv = getByTestId('test-balance-box')
+
+    expect(balanceDiv).toHaveTextContent('$0.00')
+  })
 })
-
-test("It should render positive balance in the right format", () => {
-
-  const { getByTestId } = render(<Balance userState={mockedUser}/>);
-  const balanceDiv = getByTestId('test-balance-box');
-
-  expect(balanceDiv).toBeInTheDocument();
-  expect(balanceDiv).toHaveTextContent('$1,000,000.00');
-
-})
-
-test("It should render negative balance in the right format", () => {
-
-  mockedUser.balance = -1000000;
-
-
-  const { getByTestId } = render(<Balance userState={mockedUser}/>);
-  const balanceDiv = getByTestId('test-balance-box');
-
-  expect(balanceDiv).toBeInTheDocument();
-  expect(balanceDiv).toHaveTextContent('-$1,000,000.00');
-
-})
-
-test("It should render no balance in the right format", () => {
-
-  mockedUser.balance = 0;
-
-
-  const { getByTestId } = render(<Balance userState={mockedUser}/>);
-  const balanceDiv = getByTestId('test-balance-box');
-
-  expect(balanceDiv).toBeInTheDocument();
-  expect(balanceDiv).toHaveTextContent('$0.00');
-
-})
-
-
-
-
-
