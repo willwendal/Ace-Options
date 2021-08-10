28c977458c74b8b34bcbfca4a349fd1f3d82b7c2
Alex <19alexferrante99@gmail.com>
2021-08-10 13:51:48 +0200

    balance test fixed

--- client/src/Components/Balance/Balance.component.test.js
+++ client/src/Components/Balance/Balance.component.test.js
@@ -1,6 +1,10 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
+
 import Balance from './Balance.component.jsx'
-import { render, cleanup } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+import toBeInTheDocument from '@testing-library/jest-dom'
+
 import '@testing-library/jest-dom/extend-expect'
 
 describe('Balance', () => {
@@ -10,32 +14,31 @@ describe('Balance', () => {
     balance: 1000000,
   }
 
-  afterEach(() => {
-    cleanup()
-  })
-
   test('should render positive balance in the right format', () => {
-    const { getByTestId } = render(<Balance userState={mockedUser} />)
-    const balanceDiv = getByTestId('test-balance-box')
+    render(<Balance userState={mockedUser} />)
+    const balanceDiv = screen.getByTestId('test-balance-box')
 
+    expect(balanceDiv).toBeInTheDocument();
     expect(balanceDiv).toHaveTextContent('$1,000,000.00')
   })
 
   test('should render negative balance in the right format', () => {
     mockedUser.balance = -1000000
 
-    const { getByTestId } = render(<Balance userState={mockedUser} />)
-    const balanceDiv = getByTestId('test-balance-box')
+    render(<Balance userState={mockedUser} />)
+    const balanceDiv = screen.getByTestId('test-balance-box')
 
+    expect(balanceDiv).toBeInTheDocument();
     expect(balanceDiv).toHaveTextContent('-$1,000,000.00')
   })
 
   test('should render no balance in the right format', () => {
     mockedUser.balance = 0
 
-    const { getByTestId } = render(<Balance userState={mockedUser} />)
-    const balanceDiv = getByTestId('test-balance-box')
+    render(<Balance userState={mockedUser} />)
+    const balanceDiv = screen.getByTestId('test-balance-box')
 
+    expect(balanceDiv).toBeInTheDocument();
     expect(balanceDiv).toHaveTextContent('$0.00')
   })
 })
